548d222cb5bc58bd317ac198fc18e81a1e210110
akonst <artem3845@gmail.com>
2021-05-22 12:50:32 +0300

    exceptions, correct applying of unique_ptr, bug fixes, correct iterators

--- labs/lab5/circular_buffer.h
+++ labs/lab5/circular_buffer.h
@@ -2,22 +2,52 @@
 #include <iostream>
 #include <memory>
 
-/* Данный буфер при наличии места в нем зачисляет в себя поступающие элементы, в случае заполненности
- * идет перезапись старых элементов более новыми, аналогичное происходит при удалении элементов из
- * буфера - очищаются более старые данные */
+#define ZERO_CAPACITY "You can't add an element to buffer with zero capacity! Expand the capacity!"
+#define EMPTY "Buffer is empty!"
+#define FULL "Buffer is full!"
+#define INDEX_ERROR "You're out of the buffer!";
+#define EXPAND_ERROR "Enter the correct capacity! Capacity may be an unsigned integer > 0!"
+
+#define CATCH_ERROR catch(const char* expression) {  std::cerr << "Error: " << expression << "\n"; }
 
 template <class T>
 class CircularBuffer
 {
 private:
-    size_t m_capacity;
-    size_t m_back_index = 0;
-    size_t m_front_index = 0;
-    std::unique_ptr<T> m_buff;
+    size_t capacity_, count_;
+    std::unique_ptr<T[]> data_;
 public:
-    /* constructor with capacity argument */
+    class CIterator: public std::iterator<std::random_access_iterator_tag, T>
+    {
+    private:
+        T* ptr_;
+    public:
+        CIterator(T* p): ptr_(p) {}
+        CIterator(const CIterator& other): ptr_(other.ptr_) {}
 
-    CircularBuffer(size_t cap): m_capacity(cap), m_buff(std::unique_ptr<T>(new T[cap])) {}
+        bool operator==(CIterator const& other) { return ptr_ == other.ptr_; }
+        bool operator!=(CIterator const& other) { return ptr_ != other.ptr_; }
+
+        typename CIterator::reference operator*() const
+        {
+            return *ptr_;
+        }
+
+        CIterator& operator++()
+        {
+            ++ptr_;
+            return *this;
+        }
+
+        CIterator& operator--()
+        {
+            --ptr_;
+            return *this;
+        }
+    };
+
+    CircularBuffer(): capacity_(0), count_(0) {} // constructor
+    explicit CircularBuffer(size_t cap): capacity_(cap), count_(0) {} // constructor with capacity
 
     /* we don't need a custom destructor because of "smart pointer" application */
 
@@ -25,61 +55,183 @@ public:
     void push_front(T);
     void pop_back();
     void pop_front();
-    void new_capacity();
+    void new_capacity(size_t);
     void print() const;
+    bool full_buffer() const;
 
-    T& get_front();
-    T& get_back();
-    T& operator[] (unsigned);
+    T& operator[] (size_t);
+
+    typedef CIterator iterator;
+    typedef CIterator const const_iterator;
+
+    iterator begin() { return iterator(data_.get()); }
+    iterator end() { return iterator(data_.get() + capacity_); }
+
+    const_iterator begin() const { return const_iterator(data_.get()); }
+    const_iterator end() const { return const_iterator(data_.get() + count_); }
 };
+
+
+template <typename T>
+bool CircularBuffer<T>::full_buffer() const { return capacity_ == count_; }
 
 template <typename T>
 void CircularBuffer<T>::print() const
 {
-    if (!m_capacity)
-        std::cout << "Your circular buffer is empty!";
+    if (!count_)
+        std::cout << EMPTY << "\n";
     else
     {
-        std::cout << "Your circular buffer capacity: " << m_capacity << "." << "\n";
+        std::cout << "Elements count in your circular buffer: " << count_ << "\n";
+        std::cout << "Your circular buffer capacity: " << capacity_ << "." << "\n";
         std::cout << "Your circular buffer contains these elements: " << "\n";
-        for (size_t i = 0; i < m_capacity; ++i)
+
+        auto iter = begin();
+        int i = 0;
+
+        while (iter != end())
         {
-            std::cout << i + 1 << ": " << m_buff[i] << "\n";
+            std::cout << "[" << i + 1 << "] element" << ": " << *iter << "\n";
+            ++iter;
+            ++i;
         }
+        std::cout << "\n";
     }
 }
+
+/* Механика вставки в конец: расширение буфера идет до тех пор, пока количество элементов в нем не станет равным
+ * капасити, когда это будет так - перестанем вставлять элементы, сославшись на то, что буфер полон */
 
 template <typename T>
 void CircularBuffer<T>::push_back(T element)
 {
-    m_buff[m_back_index] = element;
+    try
+    {
+        if (!capacity_)
+            throw ZERO_CAPACITY;
+        if (full_buffer())
+            throw FULL;
 
-    if (m_back_index != m_capacity - 1)
-        ++m_back_index;
-    else
-        m_back_index = 0;
+        auto temp = std::make_unique<T[]>(count_ + 1);
+
+        for (size_t i = 0; i < count_; ++i)
+            temp[i] = data_[i];
+
+        temp[count_] = element;
+        data_ = std::move(temp); // or data_.reset() then std::move?
+        ++count_;
+    }
+
+    CATCH_ERROR;
 }
 
-template <typename T>
-void CircularBuffer<T>::push_front(T element)
-{
-    m_buff[m_front_index] = element;
-
-    if (m_front_index != m_capacity - 1)
-        ++m_back_index;
-    else
-        m_back_index = 0;
-}
-
-template <typename T>
-T& CircularBuffer<T>::get_back()
-{
-    if (m_buff[m_back_index] != nullptr)
-        return m_buff[m_back_index];
-}
+/* Механика удаления из буфера: удаляем последний элемент, капасити динамической памяти уменьшаем до количество_эл - 1 */
 
 template <typename T>
 void CircularBuffer<T>::pop_back()
 {
-    m_buff[m_back_index] = nullptr;
+    try
+    {
+        if (!count_)
+            throw EMPTY;
+        if (count_ > 1)
+        {
+            auto temp = std::make_unique<T[]>(count_ - 1);
+
+            for (size_t i = 0; i < count_ - 1; ++i)
+            {
+                temp[i] = data_[i];
+            }
+
+            data_ = std::move(temp);
+        }
+        else
+            data_.reset();
+
+        --count_;
+    }
+
+    CATCH_ERROR;
+}
+
+/* Механика вставки в начало - сдвиг на элемент вправо, вставка на первое место - до полноты буфера */
+
+template <typename T>
+void CircularBuffer<T>::push_front(T element)
+{
+    try
+    {
+        if (!capacity_)
+            throw ZERO_CAPACITY;
+        if (full_buffer())
+            throw FULL;
+
+        auto temp = std::make_unique<T[]>(count_ + 1);
+
+        for (size_t i = 1; i < count_ + 1; ++i)
+            temp[i] = data_[i - 1];
+
+        temp[0] = element;
+        data_ = std::move(temp);
+        ++count_;
+    }
+
+    CATCH_ERROR;
+}
+
+/* Удаление - удаляем первый, сдвиг влево до пустоты */
+
+template <typename T>
+void CircularBuffer<T>::pop_front()
+{
+    try
+    {
+        if (!count_)
+            throw EMPTY;
+        if (count_ > 1)
+        {
+            auto temp = std::make_unique<T[]>(count_ - 1);
+
+            for (size_t i = 0; i < count_ - 1; ++i)
+            {
+                temp[i] = data_[i + 1];
+            }
+
+            data_ = std::move(temp);
+        }
+        else
+            data_.reset();
+
+        --count_;
+    }
+
+    CATCH_ERROR;
+}
+
+template <typename T>
+void CircularBuffer<T>::new_capacity(size_t new_cap)
+{
+    try
+    {
+        if (new_cap <= 0)
+            throw EXPAND_ERROR;
+
+        capacity_ = new_cap;
+    }
+
+    CATCH_ERROR;
+}
+
+template <typename T>
+T& CircularBuffer<T>::operator[] (size_t index)
+{
+    try
+    {
+        if (index < 0 || index > count_ - 1)
+            throw INDEX_ERROR;
+
+        return data_[index];
+    }
+
+    CATCH_ERROR;
 }
--- labs/lab5/main.cpp
+++ labs/lab5/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
-#include <memory>
 #include "circular_buffer.h"
 
 int main()
 {
-    std::cout << "еще ничего не готово...";
+    CircularBuffer<int> buffer_test(3);
+
+    buffer_test.push_back(5); // buff: 5
+    buffer_test.push_back(10); // buff: 5 10
+    buffer_test.push_back(15); // buff: 5 10 15
+    buffer_test.print();
+
+    buffer_test.push_back(20); // должно вызвать ошибку
+
+    buffer_test.pop_front(); // buff: 10 15
+    buffer_test.pop_back(); // buff: 10
+    buffer_test.push_back(666); // buff: 10 666
+    buffer_test.print();
+
+
+    buffer_test.push_front(555); // buff: 555 10 666
+    buffer_test.print();
+
+    std::cout << "Access to the beginning by R-A iterator: " << *(buffer_test.begin()) << "\n"; // 555
+    std::cout << "Access by index: " << buffer_test[1] << " <-- second element!\n";
+    std::cout << "Access to the end by R-A iterator: " << *(--buffer_test.end()) << "\n\n"; // 666
+
+    buffer_test.new_capacity(4);
+    buffer_test.push_back(111); // 555 10 666 111
+    buffer_test.print();
 
     return 0;
 }
